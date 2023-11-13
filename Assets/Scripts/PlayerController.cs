8883c2b1e2837955b8c2d30592d5023680dcfd27
teanbes <teanbes@gmail.com>
2023-11-13 18:32:34 -0500

    Player Controller
    - Created Player Prefab
    - Added Player controller with basic movement
    - Added JuanScene for testing
    - Added some images as place holders

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float playerSpeed = 6f;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Camera cam;
+    private float horizontalInput;
+    private float verticalInput;
+    private Vector2 playerMovement;
+    private Vector2 mousePos;
+
+   
+    private void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        playerMovement.x = horizontalInput;
+        playerMovement.y = verticalInput;
+
+        playerMovement.Normalize();
+
+        // Player movement
+        rb.MovePosition(rb.position + playerSpeed * Time.fixedDeltaTime * playerMovement);
+
+        // Calculates the vector from player position to mouse position
+        Vector2 lookDir = mousePos - rb.position;
+
+        // calculates the angle between the player and the mouse cursor 
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+    }
+
+    private void Shoot()
+    {
+
+    }
+
+}
