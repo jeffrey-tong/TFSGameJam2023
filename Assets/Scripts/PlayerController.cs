4256f60259f3e3e467cfbbd1a3bf4e0879a777d1
teanbes <teanbes@gmail.com>
2023-11-13 20:01:06 -0500

    Added dimension switching

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,67 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float playerSpeed = 6f;
+    [Header("Components")]
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Camera cam;
+
+    [Header("Player Data")]
+    [SerializeField] private float playerSpeed;
+    [SerializeField] private float fireRate;
+
+    [Header("Map Objects")]
+    [SerializeField] private GameObject dimensionMap1;
+    [SerializeField] private GameObject dimensionMap2;
+    [SerializeField] private bool isAnotherDimension;
+
     private float horizontalInput;
     private float verticalInput;
     private Vector2 playerMovement;
     private Vector2 mousePos;
+    private float lastBulletFiredTime;
 
-   
+    private void Start()
+    {
+        if (rb == null) 
+            rb = GetComponent<Rigidbody2D>();
+
+        if (cam == null) 
+            cam = Camera.main;
+
+        if (playerSpeed <= 0) 
+            playerSpeed = 6.0f;
+
+        if (fireRate <= 0)
+            fireRate = 1.0f;
+
+        lastBulletFiredTime = 1.0f;
+        isAnotherDimension = false;
+    }
+
     private void Update()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        lastBulletFiredTime += Time.deltaTime;
+
+        // Fire
+        if (Input.GetButtonDown("Fire1") && lastBulletFiredTime >= fireRate)
+        {
+            Shoot();
+            
+            lastBulletFiredTime = 0.0f;
+        }
+
+        // Split Dimension
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            SplitDimension();
+        }
+
+        
     }
 
     private void FixedUpdate()
@@ -45,6 +91,28 @@ public class PlayerController : MonoBehaviour
     }
 
     private void Shoot()
+    {
+
+        Debug.Log("shootiiiinnngggg");
+    }
+
+    private void SplitDimension()
+    {
+        if (!isAnotherDimension) 
+        { 
+            dimensionMap1.SetActive(false);
+            dimensionMap2.SetActive(true);
+            isAnotherDimension = true;
+        }
+        else
+        {
+            dimensionMap1.SetActive(true);
+            dimensionMap2.SetActive(false);
+            isAnotherDimension = false;
+        }
+    }
+
+    private void Health()
     {
 
     }
